a34f72246024e00b861af2ac81b81ff2f7081502
Yi Wang <yiwang1982@Yis-MacBook-Air.local>
2014-09-23 18:29:01 -0400

    Initial Commit

---
+++ AircraftQueueLocal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AircraftQueueLocal
+//
+//  Created by Yi Wang on 9/23/14.
+//  Copyright (c) 2014 Yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
